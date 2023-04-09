35e5a396e40e7b4b4b048dab3a7514cd2c9f5a41
Alfredo Borroto <alborroto@student.fullsail.edu>
2023-04-09 18:27:44 -0400

    Created two-way bindings for the elements on the screen and a test section to verify the changes.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginPortalComponent } from './login-portal/login-portal.component';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPortalComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
