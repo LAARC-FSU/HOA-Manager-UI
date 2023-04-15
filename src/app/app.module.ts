cffe8358a56050249ab537b8c699148249f54a39
Alfredo Borroto <alborroto@student.fullsail.edu>
2023-04-14 20:25:39 -0400

    Refactored the code for the last time and created the sign in screen, the sign up screen, and I am working on the forgot password and forgot email screen. Created some aninmations in a new file for the component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginPortalComponent } from './login-portal/login-portal.component';
 import {FormsModule} from "@angular/forms";
 
+
+
 @NgModule({
   declarations: [
     AppComponent,
-    LoginPortalComponent
+    LoginPortalComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/login-portal/login-portal.component.ts
+++ src/app/login-portal/login-portal.component.ts
@@ -1,24 +1,42 @@
 import { Component } from '@angular/core';
+import {fadeInOutAnimation} from "./animations";
 
 @Component({
+  animations: [fadeInOutAnimation],
   selector: 'login-portal',
-  templateUrl: './login-portal.component.html',
-  styleUrls: ['./login-portal.component.scss']
+  styleUrls: ['./login-portal.component.scss'],
+  templateUrl: './login-portal.component.html'
 })
 export class LoginPortalComponent {
-  mode = Mode.signup;
-  signedupUsers: any[] = [];
+  private mode = Mode.login;
+  private emailFound = '';
+  private signedupUsers: any[] = [];
 
   user= {
     type: 'employee',
     email: '',
     password: ''
   };
-  submit(){
-
+  getMode(){
+    return this.mode
+  }
+  getEmailFound(){
+    return  this.emailFound;
+  }
+  toSignUp(){
+    this.mode = Mode.signup;
+  }
+  logIn(){
+    //ToDo
   }
   exit(){
-
+  //ToDo
+  }
+  signUp(){
+    this.mode = Mode.login
+  }
+  back(){
+    this.mode = Mode.login
   }
 
   protected readonly Mode = Mode;
