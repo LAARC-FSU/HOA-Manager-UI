fa88aca6ddddafefde0ab91c436248dc96c513b1
Alfredo Borroto <alborroto@student.fullsail.edu>
2023-05-04 21:22:07 -0400

    Working on time-picker setting component id dynamically

--- src/app/make-schedule/time-picker/time-picker.component.ts
+++ src/app/make-schedule/time-picker/time-picker.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, Input} from '@angular/core';
 import { Time} from "@angular/common";
 
 @Component({
@@ -7,7 +7,7 @@ import { Time} from "@angular/common";
   styleUrls: ['./time-picker.component.scss']
 })
 export class TimePickerComponent implements OnInit {
-  panelOpen = true;
+  @Input() componentId = '';
   hours=['01','02','03','04','05','06','07','08','09','10','11','12'];
   minutes = ["00", "05", "10", "15", "20", "25", "30", "35", "40", "45", "50", "55"];
   timeStr ='';
