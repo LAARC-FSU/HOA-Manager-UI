59b116abdb73b7ec249dbbfee69d573fe5810cd2
Alfredo Borroto <alborroto@student.fullsail.edu>
2023-05-18 16:04:58 -0400

    Created a data service to share data between components.

--- src/app/schedule-view/schedule-view.component.ts
+++ src/app/schedule-view/schedule-view.component.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
-enum state{ view, make}
+import {Component} from '@angular/core';
+import {BehaviorSubject} from "rxjs";
+
+interface Shift {
+  id: string
+  start: string;
+  end: string;
+}
+
+interface Schedule {
+  timeFrame: Date[];
+  firstShiftTime: Shift;
+  secondShiftTime: Shift;
+  thirdShiftTime: Shift;
+  schedules: { [key: string]: {}[] };
+}
+
+enum state { view, make}
+
 @Component({
   selector: 'schedule-view',
   templateUrl: './schedule-view.component.html',
   styleUrls: ['./schedule-view.component.scss']
 })
 export class ScheduleViewComponent {
-currState = state.make;
-weekDaysHeader = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+  weekDaysHeader = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+  schedule: Schedule = {
+    timeFrame: [],
+    firstShiftTime: {id: 'first shift', start: '', end: ''},
+    secondShiftTime: {id: 'second shift', start: '', end: ''},
+    thirdShiftTime: {id: 'third shift', start: '', end: ''},
+    schedules: {}
+  };
+  private scheduleSource = new BehaviorSubject<Schedule>(this.schedule);
+  currSchedule = this.scheduleSource.asObservable();
 
+  updateSchedule(newSchedule: Schedule) {
+    this.scheduleSource.next(newSchedule);
+  }
 }
