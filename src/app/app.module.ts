ac2b10a4bec7dc979e927a8e3f39a3a0bbf5ed2d
Ronald Robbins <RGRobbins@student.fullsail.edu>
2023-05-20 12:48:43 -0600

    modified to put the components together

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,9 @@ import { AppComponent } from './app.component';
 import { LoginPortalComponent } from './login-portal/login-portal.component';
 import { AddPropertyComponent } from './add-property/add-property.component';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {PropertyInputFormComponent} from "./add-property/input-form/property-input-form.component";
+import {TransactionComponent} from "./add-property/transactions/transaction.component";
+import {NotesComponents} from "./add-property/notes/notes.components";
 
 
 
@@ -14,6 +17,9 @@ import {FormsModule, ReactiveFormsModule} from "@angular/forms";
     AppComponent,
     LoginPortalComponent,
     AddPropertyComponent,
+    PropertyInputFormComponent,
+    TransactionComponent,
+    NotesComponents,
   ],
     imports: [
         BrowserModule,
