f7d8cc58c5cbed0f0c94588dce8424568cc28227
Ronald Robbins <RGRobbins@student.fullsail.edu>
2023-05-20 12:47:46 -0600

    created notes section for adding property

---
+++ src/app/add-property/notes/notes.components.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import type { OnInit } from '@angular/core';
+
+@Component({
+  selector: 'notes',
+  templateUrl: './notes.component.html',
+  styleUrls: [ './notes.component.scss'],
+})
+export class NotesComponents implements OnInit {
+  constructor() {}
+  ngOnInit(): void {}
+}
