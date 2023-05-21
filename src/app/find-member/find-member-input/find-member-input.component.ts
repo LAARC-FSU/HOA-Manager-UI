0bdbd837f050865bc2c1069c40d4125b724412c7
Ronald Robbins <RGRobbins@student.fullsail.edu>
2023-05-21 13:52:52 -0600

    moved components around

--- src/app/find-member/find-member-input/find-member-input.component.ts
+++ src/app/find-member/find-member-input/find-member-input.component.ts
@@ -4,7 +4,7 @@ import type { OnInit } from '@angular/core';
 @Component({
   selector: 'find-member-input',
   templateUrl: './find-member-input.component.html',
-  styleUrls: ['./find-member-input.component.scss'],
+  styleUrls: [ './find-member-input.component.scss'],
 })
 export class FindMemberInputComponent implements OnInit {
   constructor() {}
