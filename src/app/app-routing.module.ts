0f1d74e642726c0bba7d110d78489feb89a74dd6
Ronald Robbins <RGRobbins@student.fullsail.edu>
2023-05-21 10:24:40 -0600

    added table elements to the search results table

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {SearchBoxComponent} from "./find-property/search-box/search-box.component";
 import {SearchResultComponent} from "./find-property/search-results/search-result.component";
+import {NgForOf} from "@angular/common";
 
 const routes: Routes = [];
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes), NgForOf],
   declarations: [
     SearchBoxComponent,
     SearchResultComponent
