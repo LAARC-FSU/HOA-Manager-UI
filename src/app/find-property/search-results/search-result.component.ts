7923082dcc459f96d6f0fca52499d4864c954144
Ronald Robbins <RGRobbins@student.fullsail.edu>
2023-05-21 09:32:44 -0600

    added header for search results

--- src/app/find-property/search-results/search-result.component.ts
+++ src/app/find-property/search-results/search-result.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
 import type { OnInit } from '@angular/core';
 
+class Results {
+  name:any;
+  address:any;
+  unit:any;
+  block:any;
+  lot:any;
+}
 @Component({
   selector: 'search-result',
   templateUrl: './search-result.component.html',
   styleUrls: ['./search-result.component.scss'],
 })
 export class SearchResultComponent implements OnInit {
+  results: Results[] = [{name:'Ron Robbins', address:'30 Tierra Grande', unit:'22', block:'2', lot:'17'}];
   constructor() {}
   ngOnInit(): void {}
 }
