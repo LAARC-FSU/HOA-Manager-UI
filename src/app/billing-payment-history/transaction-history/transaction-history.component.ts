eb0c2821642affd11ebe58bc8db7d5661c687283
Ronald Robbins <RGRobbins@student.fullsail.edu>
2023-05-26 11:08:55 -0600

    Added data to the transaction history section

--- src/app/billing-payment-history/transaction-history/transaction-history.component.ts
+++ src/app/billing-payment-history/transaction-history/transaction-history.component.ts
@@ -1,9 +1,17 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
+class History {
+  Date:any;
+  Amount:any;
+  For:any;
+  Method:any;
+}
 @Component({
   selector: 'transaction-history',
   styleUrls: ['./transaction-history.component.scss'],
   templateUrl: './transaction-history.component.html'
 })
+export class TransactionHistoryComponent{
+  results: History[] = [{Date: '11/2/22', Amount: '$100.00', For: 'HOA Dues', Method: 'Check'}];
+}
 
-export class TransactionHistoryComponent{};
