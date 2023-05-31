bfd5abd321fcd0eafa5898753fa03dffba2dc43a
Ronald Robbins <RGRobbins@student.fullsail.edu>
2023-05-31 14:14:55 -0600

    created list of properties components

--- src/app/member-view-property/member-view-property.component.ts
+++ src/app/member-view-property/member-view-property.component.ts
@@ -1,5 +1,11 @@
 import { Component} from "@angular/core";
 
+class Results {
+  unit:any;
+  block:any;
+  lot:any;
+  acres:any;
+}
 @Component({
   selector: 'member-view-property',
   templateUrl: 'member-view-property.component.html',
@@ -7,5 +13,5 @@ import { Component} from "@angular/core";
 })
 
 export class MemberViewPropertyComponent {
-
+  results: Results[] = [{unit:'22', block:'2', lot:'17', acres:'23.4'}];
 }
