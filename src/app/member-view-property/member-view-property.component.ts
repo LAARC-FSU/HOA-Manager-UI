f855f94e6b4b53e784ad941baf2724e82843fa83
Ronald Robbins <RGRobbins@student.fullsail.edu>
2023-05-30 18:33:08 -0600

    created member view property files

---
+++ src/app/member-view-property/member-view-property.component.ts
@@ -0,0 +1,11 @@
+import { Component} from "@angular/core";
+
+@Component({
+  selector: 'member-view-property',
+  templateUrl: 'member-view-property.component.html',
+  styleUrls: ['member-view-property.component.scss']
+})
+
+export class MemberViewPropertyComponent {
+
+}
